6a3e51ee9e350b34c18981dd813ec2bdff46691f
Boris Rosenov <boris.rosenov@paysafe.com>
2016-08-19 10:27:23 +0300

    added subject form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,6 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import {CreateInvoiceComponent} from './create-invoice.component';
 
 export const routes: RouterConfig = [
-  { path: 'invoice/create', component: CreateInvoiceComponent }
 ];
 
 export const appRouterProvider = [provideRouter(routes)];
